a532d16c59710521c197b3d16ab423421aa2731c
M. Afkar Siddiq <m.afkar.20@mhs.unsyiah.ac.id>
2022-04-22 11:18:55 +0700

    Merapikan bagian HTML praktikum

--- headanatomi/lib/main.dart
+++ headanatomi/lib/main.dart
@@ -1,129 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyWidget(),   
+    )
+  );
+}
 
-class MyApp extends StatelessWidget {
+Widget imageSection = Container(
+  child: Image.asset('images/Skull.png'),
+);
+
+Widget textButton = TextButton (
+    style: TextButton.styleFrom(
+      primary: Colors.blue,
+    ),
+  onPressed: () {},
+  child: Text('Next'),
+);
+
+class MyWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-
-    Widget imageSection = Container(
-      child: Image.asset('images/Skull.png'),
-    );
-
-    Widget titleSection = Container(
-      padding: EdgeInsets.only(top: 16),
-      child: Text(
-        'Skull',
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 18,
-        ),
-      ),
-    ); 
-
-    Widget descriptionSection = Container(
-      padding: EdgeInsets.all(16),
-    );
-
-    Widget rateSection = Row(
-      children: <Widget>[
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star),
-      ],
-    );
-
-    Widget reviewSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: <Widget>[
-        rateSection,
-        Text('170 Reviews'),
-      ],
-    );
-
-    Widget menuSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        _buildMenuSection(
-          Icons.bookmark,
-          "Prep", 
-          "25 min"
-          ),
-          _buildMenuSection(
-          Icons.timer,
-          "Cook", 
-          "1 hour"
-          ),
-          _buildMenuSection(
-          Icons.fastfood,
-          "Feeds", 
-          "4-6"
-          ),
-      ],
-    ); 
-
-    return MaterialApp(
-      title: 'Material Apps',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(144, 178, 89, 219),
-          title: Text(
-            'Welcome to'
-            'HeadAnatomy',
-            style: TextStyle(color: Color(0xFF000000)),
-          ),
-          actions: <Widget>[
-            Icon(Icons.search, color: Colors.black,)
-          ],
-        ),
-        body: ListView(
-          children: [
-            imageSection,
-            titleSection,
-            descriptionSection,
-            Container(
-              padding: EdgeInsets.only(bottom: 24),
-              child: reviewSection,
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar : AppBar(title: Text("HeadAnatomy")),
+      body: ListView(
+        children: [
+          imageSection,
+          textButton,
+          Container(
+            alignment: Alignment.bottomCenter
             ),
-            menuSection,
-          ],
-        ),
-      ),
+        ],     
+      )
     );
   }
-}
-
-Widget _buildTextSection(
-  String text, 
-  double textSize,
-  double paddingTop,
-) {
-  return Container(
-    padding: EdgeInsets.only(top: paddingTop),
-    child: Text(
-      text,
-      style: TextStyle(
-        fontSize: textSize,
-      ),
-    ),
-  );
-}
-
-Widget _buildMenuSection(
-  IconData iconData,
-  String title,
-  String timestamp,
-) {
-
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Icon(iconData),
-      _buildTextSection(title, 16, 8),
-      _buildTextSection(timestamp, 12, 12),
-    ],
-  );
 }
