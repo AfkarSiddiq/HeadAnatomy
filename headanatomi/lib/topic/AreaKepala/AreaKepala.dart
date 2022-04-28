92c7a82ad125d79b4eac9f9d6bf5c17d19c1959f
M. Afkar Siddiq <m.afkar.20@mhs.unsyiah.ac.id>
2022-04-29 05:40:45 +0700

    Make some subtopic file

---
+++ headanatomi/lib/topic/AreaKepala/AreaKepala.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:from_css_color/from_css_color.dart';
+import 'package:headanatomi/topic/AreaKepala/FossaCranial/FossaCranial.dart';
+import 'package:headanatomi/topic/home_view.dart';
+import 'package:headanatomi/topic/subject.dart';
+import 'package:headanatomi/topic/subject2.dart';
+import 'package:headanatomi/topic/topic.dart';
+
+class AreaKepala extends StatefulWidget {
+  const AreaKepala({ Key? key }) : super(key: key);
+
+  @override
+  State<AreaKepala> createState() => _AreaKepalaState();
+}
+
+class _AreaKepalaState extends State<AreaKepala> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Area Kepala"),
+          backgroundColor: fromCssColor("#00bcd4"),        
+        ),
+      body: Container(
+        color: fromCssColor('#00bcd4'),
+        child: ListView(
+          padding: EdgeInsets.all(32),
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 353,
+                  height: 100,
+                  decoration: new BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: new BorderRadius.circular(20.0),
+                  ),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: fromCssColor('#ffffff'),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )
+                    ), 
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FossaCranial()),
+                        );
+                    },
+                    child: Text(
+                      "Fossa Cranial",
+                      textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          color: fromCssColor('#000000'),
+                        ),
+                    ),
+                  )
+                ),
+                emptySpace,
+                 Container(
+                  width: 353,
+                  height: 100,
+                  decoration: new BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: new BorderRadius.circular(20.0),
+                  ),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: fromCssColor('#ffffff'),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )
+                    ), 
+                    onPressed: () {},
+                    child: Text(
+                      "Fossa Mastoid",
+                      textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          color: fromCssColor('#000000'),
+                        ),
+                    ),
+                  )
+                ),
+                emptySpace,
+                 Container(
+                  width: 353,
+                  height: 100,
+                  decoration: new BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: new BorderRadius.circular(20.0),
+                  ),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: fromCssColor('#ffffff'),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )
+                    ), 
+                    onPressed: () {},
+                    child: Text(
+                      "Kulit Kepala",
+                      textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          color: fromCssColor('#000000'),
+                        ),
+                    ),
+                  )
+                ),
+                emptySpace,
+                 Container(
+                  width: 353,
+                  height: 100,
+                  decoration: new BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: new BorderRadius.circular(20.0),
+                  ),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: fromCssColor('#ffffff'),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )
+                    ), 
+                    onPressed: () {},
+                    child: Text(
+                      "Fossa Infratemporal",
+                      textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          color: fromCssColor('#000000'),
+                        ),
+                    ),
+                  )
+                ),
+                emptySpace,
+                 Container(
+                  width: 353,
+                  height: 100,
+                  decoration: new BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: new BorderRadius.circular(20.0),
+                  ),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: fromCssColor('#ffffff'),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )
+                    ), 
+                    onPressed: () {},
+                    child: Text(
+                      "Fossa Pterigopalatina",
+                      textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          color: fromCssColor('#000000'),
+                        ),
+                    ),
+                  )
+                ),
+                emptySpace,
+              ],
+            )
+          ],
+      )
+    )));
+  }
+}
---
+++ headanatomi/lib/topic/AreaKepala/FossaCranial/FossaCranialAnterior.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:from_css_color/from_css_color.dart';
+import 'package:headanatomi/topic/home_view.dart';
+import 'package:headanatomi/topic/subject.dart';
+import 'package:headanatomi/topic/subject2.dart';
+
+
+class FossaCranialAnterior extends StatefulWidget {
+  const FossaCranialAnterior({ Key? key }) : super(key: key);
+
+  @override
+  State<FossaCranialAnterior> createState() => _FossaCranialAnteriorState();
+}
+
+class _FossaCranialAnteriorState extends State<FossaCranialAnterior> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
