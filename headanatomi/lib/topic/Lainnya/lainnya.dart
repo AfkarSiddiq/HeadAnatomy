9a9e5d07609b8b90f0af6d64d19cbae03a53d577
M. Afkar Siddiq <m.afkar.20@mhs.unsyiah.ac.id>
2022-05-05 02:58:51 +0700

    + SubTopic file

---
+++ headanatomi/lib/topic/Lainnya/lainnya.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:headanatomi/topic/topic.dart';
+import 'package:from_css_color/from_css_color.dart';
+
+class lainnya extends StatefulWidget {
+  const lainnya({ Key? key }) : super(key: key);
+
+  @override
+  State<lainnya> createState() => _lainnyaState();
+}
+
+class _lainnyaState extends State<lainnya> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Lainnya'),
+          backgroundColor: fromCssColor("#00bcd4"),
+        ),
+        body: Container(
+          color: fromCssColor('#4A8592'),
+          child: ListView(
+            padding: EdgeInsets.all(32),
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 353,
+                  height: 100,
+                  decoration: new BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: new BorderRadius.circular(20.0),
+                  ),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: fromCssColor('#ffffff'),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )
+                    ), 
+                    onPressed: () {},
+                    child: Text(
+                      "Gigi Anak dan Dewasa",
+                      textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          color: fromCssColor('#000000'),
+                        ),
+                    ),
+                  ),
+                  ),
+                  emptySpace,
+                  Container(
+                    width: 353,
+                  height: 100,
+                  decoration: new BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: new BorderRadius.circular(20.0),
+                  ),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: fromCssColor('#ffffff'),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )
+                    ), 
+                    onPressed: () {},
+                    child: Text(
+                      "Langit-langit",
+                      textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          color: fromCssColor('#000000'),
+                        ),
+                    ),
+                  ),
+                  ),
+                  emptySpace,  
+                ]
+              )
+            ]
+          )
+        ),
+      ),
+    );
+  }
+}
