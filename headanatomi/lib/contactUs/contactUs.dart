8632d198f9032a1be25a754f752dede305ebec3a
athiya29 <athiya.nf@mhs.unsyiah.ac.id>
2022-05-21 01:41:32 +0700

    perbaiki contact us

--- headanatomi/lib/contactUs/contactUs.dart
+++ headanatomi/lib/contactUs/contactUs.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:headanatomi/topic/topic.dart';
 
 class contactus extends StatefulWidget {
-  const contactus({ Key? key }) : super(key: key);
+const contactus({ Key? key }) : super(key: key);
 
   @override
   State<contactus> createState() => _contactusState();
@@ -31,7 +32,7 @@ class _contactusState extends State<contactus> {
          Card(
             child: ListTile(
               leading: Icon(
-                Icons.email, size: 32, color: Colors.blue,),
+                Icons.email, size: 32, color: Colors.blueGrey,),
 
               title: Text ('athiyanurulfadhila@gmail.com', style: TextStyle(fontSize: 20, color: Colors.black),
               ),
@@ -41,15 +42,28 @@ class _contactusState extends State<contactus> {
            Card(
             child: ListTile(
               leading: Icon(
-                Icons.phone, size: 32, color: Colors.blue,),
+                Icons.phone, size: 32, color: Colors.blueGrey,),
 
             title: Text ('+62-82354661837', style: TextStyle(fontSize: 20, color: Colors.black),
               ),
             ),
         ),
+
+        emptySpace,
+        Container(
+          //make addres
+          padding: EdgeInsets.all(10),
+          child: Text('Jl. Raya Cibadak No.1, Cibadak, Kec. Cibadak, Kota Bandung, Jawa Barat 40132', style: TextStyle(fontSize: 20, color: Colors.black),
+          textAlign: TextAlign.center,
+          
+          ),
+        ),
+        Container(
+          child: Text('thanks for choosing us', style: TextStyle(color:Colors.grey, fontSize: 30, backgroundColor: Colors.black),),
+        )   
           ] 
         )
-      )
-    );
-  }
+        )
+      );
+}
 }
